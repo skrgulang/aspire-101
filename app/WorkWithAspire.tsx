667f1598b6e9a74076a8f590642efe5aaa5fdcc1
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-04 19:22:49 -0400

    Expand campus ambassador rail

--- app/WorkWithAspire.tsx
+++ app/WorkWithAspire.tsx
@@ -5,6 +5,31 @@ const campuses = [
     credit: 'Mizzlbrd · CC BY-SA 4.0'
   },
   {
+    school: 'Purdue',
+    image: 'https://commons.wikimedia.org/wiki/Special:FilePath/Purdue%20EngineeringMall.jpg?width=900',
+    credit: 'Tstuddud · Public domain'
+  },
+  {
+    school: 'Rutgers',
+    image: 'https://commons.wikimedia.org/wiki/Special:FilePath/Queens%20Campus%20of%20Rutgers%20University%202026f.jpg?width=900',
+    credit: 'Antony-22 · CC BY-SA 4.0'
+  },
+  {
+    school: 'UIUC',
+    image: 'https://commons.wikimedia.org/wiki/Special:FilePath/Altgeld%20Hall.jpg?width=900',
+    credit: 'Daniel Schwen · CC BY-SA 4.0'
+  },
+  {
+    school: 'Ohio State',
+    image: 'https://commons.wikimedia.org/wiki/Special:FilePath/University%20Hall%20%28Ohio%20State%20University%29.jpg?width=900',
+    credit: 'Library of Congress · Public domain'
+  },
+  {
+    school: 'Michigan',
+    image: 'https://commons.wikimedia.org/wiki/Special:FilePath/Law%20Quadrangle%2C%20University%20of%20Michigan%2C%20University%20Avenue%20and%20State%20Street%2C%20Ann%20Arbor%2C%20MI%20-%2054381553310.jpg?width=900',
+    credit: 'w_lemay · CC BY-SA 2.0'
+  },
+  {
     school: 'UCLA',
     image: 'https://commons.wikimedia.org/wiki/Special:FilePath/Royce%20Hall.jpg?width=900',
     credit: 'Eric Chan · CC BY 2.0'
@@ -23,11 +48,6 @@ const campuses = [
     school: 'UC San Diego',
     image: 'https://commons.wikimedia.org/wiki/Special:FilePath/UC%20San%20Diego%20Geisel%20Library.jpg?width=900',
     credit: 'Westxtk · CC BY-SA 4.0'
-  },
-  {
-    school: 'Purdue',
-    image: 'https://commons.wikimedia.org/wiki/Special:FilePath/Purdue%20EngineeringMall.jpg?width=900',
-    credit: 'Tstuddud · Public domain'
   }
 ];
 
