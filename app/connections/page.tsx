813d8510a4052a6e27baefcf4e890ca8492078f3
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-05 19:46:23 -0400

    Add payment workspace below connections

--- app/connections/page.tsx
+++ app/connections/page.tsx
@@ -1,10 +1,14 @@
 import ConnectionsHub from '../ConnectionsHub';
+import ConnectionPaymentsPanel from '../ConnectionPaymentsPanel';
 import AppDock from '../AppDock';
 
 export default function ConnectionsPage() {
   return (
     <main className="connectionsPage">
-      <div className="connectionsShell shell"><ConnectionsHub /></div>
+      <div className="connectionsShell shell">
+        <ConnectionsHub />
+        <ConnectionPaymentsPanel />
+      </div>
       <AppDock active="connections" />
     </main>
   );
