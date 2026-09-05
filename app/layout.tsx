682c527bbad91cd38d7ac3f2a7a4665f225162d4
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-05 17:37:09 -0400

    Load Campus Circle polish styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ import './auth.css';
 import './post.css';
 import './campus-picker.css';
 import './campus-home.css';
+import './campus-circle-v2.css';
 import './marketing-home.css';
 import './marketing-story.css';
 import './marketing-vibe.css';
