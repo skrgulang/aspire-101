b8e9b61c470156927959d0c47fb52d9cace2a11f
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-07 18:27:51 -0400

    Add refund decisions to moderator console

--- app/moderator/page.tsx
+++ app/moderator/page.tsx
@@ -1,6 +1,7 @@
 import ModeratorConsole from '../ModeratorConsole';
 import DisputeIntelligenceLauncher from '../DisputeIntelligenceLauncher';
+import RefundReviewPanel from '../RefundReviewPanel';
 
 export default function ModeratorPage() {
-  return <><ModeratorConsole /><DisputeIntelligenceLauncher /></>;
+  return <><ModeratorConsole /><RefundReviewPanel /><DisputeIntelligenceLauncher /></>;
 }
