e4899ba661313c102cf043afbe4c00325916cb7b
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-07 14:30:13 -0400

    Add optional AI Connection Copilot

--- app/connections/page.tsx
+++ app/connections/page.tsx
@@ -1,6 +1,7 @@
 import ConnectionsHub from '../ConnectionsHub';
 import MarketOrdersPanel from '../MarketOrdersPanel';
 import ConnectionPaymentsPanel from '../ConnectionPaymentsPanel';
+import ConnectionCopilotPanel from '../ConnectionCopilotPanel';
 import AppDock from '../AppDock';
 
 export default function ConnectionsPage() {
@@ -8,6 +9,7 @@ export default function ConnectionsPage() {
     <main className="connectionsPage">
       <div className="connectionsShell shell">
         <ConnectionsHub />
+        <ConnectionCopilotPanel />
         <MarketOrdersPanel />
         <ConnectionPaymentsPanel />
       </div>
