290aeeaf45d6cc5acbdc1cdeb27e16fbd8f7b90c
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-08 02:29:13 -0400

    Add embedded payout setup page

---
+++ app/payments/setup/page.tsx
@@ -0,0 +1,9 @@
+import AspireEmbeddedPayoutSetup from '../../AspireEmbeddedPayoutSetup';
+
+export default function PayoutSetupPage() {
+  return (
+    <main className="embeddedPaymentPage">
+      <AspireEmbeddedPayoutSetup />
+    </main>
+  );
+}
