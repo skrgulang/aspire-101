23f124fe44b775f9e58a156a2aa3294d761c0a46
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-09 16:26:20 -0400

    Replace cluttered connections page with beta inbox

--- app/connections/page.tsx
+++ app/connections/page.tsx
@@ -1,17 +1,11 @@
-import ConnectionsHub from '../ConnectionsHub';
-import MarketOrdersPanel from '../MarketOrdersPanel';
-import ConnectionPaymentsPanel from '../ConnectionPaymentsPanel';
-import ConnectionCopilotPanel from '../ConnectionCopilotPanel';
+import BetaInbox from '../BetaInbox';
 import AppDock from '../AppDock';
 
 export default function ConnectionsPage() {
   return (
     <main className="connectionsPage">
       <div className="connectionsShell shell">
-        <ConnectionsHub />
-        <ConnectionCopilotPanel />
-        <MarketOrdersPanel />
-        <ConnectionPaymentsPanel />
+        <BetaInbox />
       </div>
       <AppDock active="connections" />
     </main>
