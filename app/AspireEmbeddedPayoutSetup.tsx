45c6dceaa457a3b8c2f93a33a88039801209049b
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-09 14:21:03 -0400

    Use progressive Stripe payout onboarding

--- app/AspireEmbeddedPayoutSetup.tsx
+++ app/AspireEmbeddedPayoutSetup.tsx
@@ -91,7 +91,7 @@ export default function AspireEmbeddedPayoutSetup() {
       <div className="embeddedPayoutFrame">
         <ConnectComponentsProvider connectInstance={connectInstance}>
           <ConnectAccountOnboarding
-            collectionOptions={{ fields: 'eventually_due' }}
+            collectionOptions={{ fields: 'currently_due' }}
             onExit={() => window.location.assign('/profile?payments=return')}
           />
         </ConnectComponentsProvider>
