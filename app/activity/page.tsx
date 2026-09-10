efe0d5de503e625980e1e0b9b22d83fb5281908b
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-10 02:50:08 -0400

    Keep My Activity focused on requests and connections

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -1,17 +1,11 @@
 import ConnectionsHub from '../ConnectionsHub';
-import MarketOrdersPanel from '../MarketOrdersPanel';
 import AppDock from '../AppDock';
 
 export default function ActivityPage() {
   return (
     <main className="connectionsPage">
       <div className="connectionsShell shell">
-        <div id="my-activity">
-          <ConnectionsHub />
-        </div>
-        <div id="transactions">
-          <MarketOrdersPanel />
-        </div>
+        <ConnectionsHub />
       </div>
       <AppDock active="activity" />
     </main>
