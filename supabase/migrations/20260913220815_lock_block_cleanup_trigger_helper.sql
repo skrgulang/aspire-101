b18edbef81349446bb8aacb806c3fc50e08909f1
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-13 18:08:54 -0400

    Lock block cleanup trigger helper

---
+++ supabase/migrations/20260913220815_lock_block_cleanup_trigger_helper.sql
@@ -0,0 +1,2 @@
+revoke all on function public.clear_circle_choices_after_block() from public, anon, authenticated;
+grant execute on function public.clear_circle_choices_after_block() to service_role;
