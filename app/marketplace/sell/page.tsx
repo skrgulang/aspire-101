b406881b333aa40a84e113f0f4134ea98bd22ca0
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-14 16:56:48 -0400

    Add seller marketplace route

---
+++ app/marketplace/sell/page.tsx
@@ -0,0 +1,10 @@
+import MarketplaceSellForm from './MarketplaceSellForm';
+
+export const metadata = {
+  title: 'Sell on Aspire Market · Aspire 101',
+  description: 'Create a marketplace listing and choose one or more fulfillment methods.'
+};
+
+export default function MarketplaceSellPage() {
+  return <MarketplaceSellForm />;
+}
