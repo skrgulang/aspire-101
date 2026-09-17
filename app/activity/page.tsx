1f16ef5a1b1141972b0665c8a77e18faf64cb1fe
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-17 16:47:40 -0400

    Show review submission confirmation in activity

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -1,9 +1,11 @@
+import ActivitySubmissionNotice from '../ActivitySubmissionNotice';
 import AppDock from '../AppDock';
 import MyActivityManager from '../MyActivityManager';
 
 export default function ActivityPage() {
   return (
     <main className="connectionsPage">
+      <ActivitySubmissionNotice />
       <MyActivityManager />
       <AppDock active="activity" />
     </main>
