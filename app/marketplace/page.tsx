0fe4c19c0822d1ee9e7702cf78d21b169e2377e4
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-17 02:54:58 -0400

    Finish Market page guidance and polish
    * Add complete Market guidance section
    
    * Style complete Market guidance section
    
    * Finish Market page with buyer and seller guidance

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -1,6 +1,12 @@
 import '../marketplace-v4.css';
 import MarketplaceCheckoutV4 from '../MarketplaceCheckoutV4';
+import MarketCompleteGuide from '../MarketCompleteGuide';
 
 export default function MarketplacePage() {
-  return <MarketplaceCheckoutV4 />;
+  return (
+    <>
+      <MarketplaceCheckoutV4 />
+      <MarketCompleteGuide />
+    </>
+  );
 }
