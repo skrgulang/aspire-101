09d90039c87563c337acc1c3c054e9a56ac34fe2
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-17 12:35:34 -0400

    Add smart campus context to Market

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -1,10 +1,14 @@
 import '../marketplace-v4.css';
 import MarketplaceCheckoutV5 from '../MarketplaceCheckoutV5';
 import MarketCompleteGuide from '../MarketCompleteGuide';
+import SmartCampusContextBar from '../SmartCampusContextBar';
 
 export default function MarketplacePage() {
   return (
     <>
+      <div style={{ margin: '0 auto', width: 'min(1480px, calc(100% - 120px))', paddingTop: 20 }}>
+        <SmartCampusContextBar label="SHOPPING NEAR" />
+      </div>
       <MarketplaceCheckoutV5 />
       <MarketCompleteGuide />
     </>
