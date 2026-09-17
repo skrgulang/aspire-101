e5cc4e96a27bf404474219ff9a0085b3a5fde8d5
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-17 12:43:24 -0400

    Allow same-origin geolocation for smart campus

--- next.config.mjs
+++ next.config.mjs
@@ -26,7 +26,7 @@ const nextConfig = {
         { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
         { key: 'X-Content-Type-Options', value: 'nosniff' },
         { key: 'X-Frame-Options', value: 'DENY' },
-        { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()' },
+        { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=(self), browsing-topics=()' },
         { key: 'Cross-Origin-Opener-Policy', value: 'same-origin-allow-popups' },
         { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' }
       ]
