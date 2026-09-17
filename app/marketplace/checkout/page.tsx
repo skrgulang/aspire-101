55fccc0fc7701a14252a5a376b2445dfdce69bf9
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-17 02:33:51 -0400

    Add dedicated marketplace checkout route

---
+++ app/marketplace/checkout/page.tsx
@@ -0,0 +1,9 @@
+import '../../marketplace-modal-theme.css';
+import '../../marketplace-checkout-v2.css';
+import '../../marketplace-checkout-v3.css';
+import '../../marketplace-browse-v4.css';
+import MarketplaceCheckoutV3 from '../../MarketplaceCheckoutV3';
+
+export default function MarketplaceCheckoutPage() {
+  return <div className="marketCheckoutOnly"><MarketplaceCheckoutV3 /></div>;
+}
