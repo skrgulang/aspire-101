1f8dc4695397ec47809d33e09b376051e1237929
Congyu Zhao <24zhaocongyu@gmail.com>
2026-09-17 23:31:20 -0400

    Stage safe support feedback RPCs before lock

--- supabase/migrations/20260918034500_protect_support_feedback_privacy.sql
+++ supabase/migrations/20260918034500_protect_support_feedback_privacy.sql
@@ -73,5 +73,3 @@ revoke all on function public.moderator_fetch_support_feedback(integer) from pub
 grant execute on function public.get_public_support_feedback(integer) to anon, authenticated, service_role;
 grant execute on function public.get_my_support_feedback(integer) to authenticated, service_role;
 grant execute on function public.moderator_fetch_support_feedback(integer) to authenticated, service_role;
-
-revoke select on table public.support_feedback from anon, authenticated;
